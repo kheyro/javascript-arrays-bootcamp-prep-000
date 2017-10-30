d1919760ac2fab9bf455e9b61baea6b3b982918d
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 08:59:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,31 +8,3 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
 console.log(destructivelyAddElementToBeginningOfArray([1], 'foo'));
-
-function addElementToEndOfArray(array, el) {
-  return [...array, el];
-}
-
-function destructivelyAddElementToEndOfArray(array, el) {
-  return array.push(el);
-}
-
-function accessElementInArray(array, index) {
-  return array[index];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
-}
-
-function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
-}
-
-function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop();
-}
-
-function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1);
-}
