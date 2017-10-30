f5982aaaf5411438cdceb0a1d1d428ce68860232
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 08:14:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, el){
+  return [el, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, el){
+  return array.unshift(el);
+}
